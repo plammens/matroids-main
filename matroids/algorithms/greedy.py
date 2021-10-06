c7343fb91995b9b700329a1348df0eab1f54a9a8
Paolo Lammens <lammenspaolo@gmail.com>
2021-10-06 17:52:23 +0200

    feat: Implement greedy algorithm

---
+++ matroids/algorithms/greedy.py
@@ -0,0 +1,23 @@
+"""The greedy algorithm for finding the maximal independent set of a matroid"""
+import typing
+
+from matroids.matroid import WeightedMatroid, T
+
+
+def maximal_independent_set(matroid: WeightedMatroid[T]) -> typing.Set[T]:
+    """
+    Use the greedy algorithm to find the maximal independent set of a matroid.
+
+    :param matroid: Weighted matroid of which to find the maximal independent set.
+    :return: The maximal independent set of the given matroid.
+    """
+    # sort elements in descending order of weight
+    elements = sorted(matroid.ground_set, key=matroid.get_weight, reverse=True)
+
+    current_set = set()
+    for element in elements:
+        if matroid.is_independent(current_set | {element}):
+            current_set.add(element)
+
+    return current_set
+
