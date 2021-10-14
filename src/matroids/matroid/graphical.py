f5ddb5ba1ef968954fcab01e0fd56fd1481271c4
Paolo Lammens <lammenspaolo@gmail.com>
2021-10-14 16:03:42 +0200

    feat: Implement stateful incremental independence check for graphical matroids

--- src/matroids/matroid/graphical.py
+++ src/matroids/matroid/graphical.py
@@ -1,4 +1,5 @@
 import dataclasses
+import itertools
 import typing
 
 from .base import Matroid
@@ -32,6 +33,27 @@ class GraphicalMatroid(Matroid[EdgeType]):
         if not subgraph:
             return True  # special case for empty graph; otherwise nx exception
         return nx.algorithms.tree.is_forest(subgraph)
+
+    def is_independent_incremental_stateful(
+        self, independent_subset: typing.MutableSet[EdgeType]
+    ) -> typing.Generator[bool, EdgeType, None]:
+        # initialise a disjoint-set data structure
+        # for determining the connected component that a given node belongs to
+        initial_subgraph = self.graph.edge_subgraph(independent_subset)
+        node_to_connected_component = nx.utils.UnionFind(initial_subgraph.nodes)
+        connected_components = nx.algorithms.connected_components(initial_subgraph)
+        for c in connected_components:
+            node_to_connected_component.union(*c.nodes)
+
+        # the generator
+        is_independent = True
+        while True:
+            u, v = new_edge = (yield is_independent)
+            # the following check is amortized O(1)
+            if node_to_connected_component[u] != node_to_connected_component[v]:
+                # the edge connects different connected components, so we can add it
+                is_independent = True
+                independent_subset.add(new_edge)
 
     def get_weight(self, element: EdgeType) -> float:
         weight = self.graph.get_edge_data(*element).get("weight", 1.0)
