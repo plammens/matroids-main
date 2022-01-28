9cd1b1122a03d57803eee6ac772e437674bc77de
Paolo Lammens <lammenspaolo@gmail.com>
2022-01-28 11:18:37 +0100

    refactor: Allow arbitrary collections in generate_subsets

--- src/matroids/utils.py
+++ src/matroids/utils.py
@@ -1,14 +1,16 @@
 import itertools
-from typing import FrozenSet, Iterable, Iterator, Optional, Set, Union
+from typing import Collection, FrozenSet, Iterable, Iterator, Optional
 
 
 def generate_subsets(
-    s: Union[Set, FrozenSet], sizes: Optional[Iterable[int]] = None
+    s: Collection, sizes: Optional[Iterable[int]] = None
 ) -> Iterator[FrozenSet]:
     """
     Generate all subsets of a set of the given sizes, in ascending order of size.
 
-    :param s: Set from which to generate subsets.
+    :param s: Set from which to generate subsets. Typed as an arbitrary collection
+        for convenience, but as a precondition it should look like a set (i.e. not have
+        any duplicates).
     :param sizes: Sizes of the subsets to generate; by default, all possible sizes.
     :return: All subsets of each of the given sizes.
     """
