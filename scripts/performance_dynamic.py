451621ebb0d1d3e3d0c6e13f07cb4d6ec0ee4a06
Paolo Lammens <lammenspaolo@gmail.com>
2022-01-31 12:28:36 +0100

    feat: Custom better performance_dynamic.py

--- scripts/performance_dynamic.py
+++ scripts/performance_dynamic.py
@@ -1,17 +1,22 @@
+import dataclasses
+import functools
 import itertools
-from typing import Generator, List, Tuple
+import typing as tp
 
 import matplotlib.pyplot as plt
+import matplotx
 import networkx as nx
 import numpy as np
-import perfplot
+import tqdm
 
 from matroids.algorithms.dynamic import (
+    DynamicMaximalIndependentSetAlgorithm,
     dynamic_removal_maximal_independent_set,
     dynamic_removal_maximal_independent_set_uniform_weights,
 )
 from matroids.algorithms.static import maximal_independent_set
-from matroids.matroid import EdgeType, GraphicalMatroid, MutableMatroid
+from matroids.matroid import EdgeType, GraphicalMatroid
+from utils.stopwatch import Stopwatch
 
 
 rng = np.random.default_rng(seed=2022)
@@ -21,8 +26,6 @@ def generate_graphical_matroid(
     size: int, rank: int, uniform_weights: bool = True
 ) -> GraphicalMatroid:
     """Generate an arbitrary graphical matroid of the given size and rank."""
-    rng = np.random.default_rng(seed=2022)  # local rng for deterministic results
-
     # start with an underlying tree/forest with a number of edges equal to the rank
     graph: nx.Graph = nx.path_graph(n=rank + 1)
     assert len(graph.edges) == rank
@@ -45,87 +48,132 @@ def generate_graphical_matroid(
     return matroid
 
 
-SetupData = Tuple[
-    List[EdgeType],
-    MutableMatroid,
-    MutableMatroid,
-    Generator,
-    MutableMatroid,
-    Generator,
-]
-
-
-def setup(size: int, rank: int, *, uniform_weights: bool) -> SetupData:
-    matroid_copy1 = generate_graphical_matroid(size, rank, uniform_weights)
-    matroid_copy2 = GraphicalMatroid(matroid_copy1.graph.copy())
-    matroid_copy3 = GraphicalMatroid(matroid_copy1.graph.copy())
-
-    # start generators for the dynamic algorithms so as to only benchmark
-    # the dynamic removal steps
-    remover2 = dynamic_removal_maximal_independent_set(matroid_copy2)
-    remover2.send(None)
-    remover3 = dynamic_removal_maximal_independent_set_uniform_weights(matroid_copy3)
-    remover3.send(None)
+def setup(
+    size: int, rank: int, *, uniform_weights: bool
+) -> tp.Tuple[GraphicalMatroid, tp.List[EdgeType]]:
+    matroid = generate_graphical_matroid(size, rank, uniform_weights)
 
     # select the sequence of removals (over all elements of the matroid)
-    elements_to_remove = list(matroid_copy1.ground_set)
-    rng.shuffle(elements_to_remove)
+    removal_sequence = list(matroid.ground_set)
+    rng.shuffle(removal_sequence)
 
-    return (
-        elements_to_remove,
-        matroid_copy1,
-        matroid_copy2,
-        remover2,
-        matroid_copy3,
-        remover3,
-    )
+    return matroid, removal_sequence
 
 
-def restart_greedy(setup_data: SetupData):
-    elements_to_remove, matroid, *_ = setup_data
-    results = []
-    for element in elements_to_remove:
-        matroid.remove_element(element)
-        results.append(maximal_independent_set(matroid))
-    return results
+def time_restart_greedy(*args, **kwargs) -> float:
+    """Time one run of the greedy algorithm; return time in nanoseconds."""
+    matroid, removal_sequence = setup(*args, **kwargs)
+
+    with Stopwatch() as stopwatch:
+        for element in removal_sequence:
+            matroid.remove_element(element)
+            maximal_independent_set(matroid)
+
+    return stopwatch.measurement
 
 
-def naive_dynamic(setup_data: SetupData):
-    elements_to_remove, _, _, remover_generator, *_ = setup_data
-    return [remover_generator.send(element) for element in elements_to_remove]
+def time_dynamic(
+    algorithm: DynamicMaximalIndependentSetAlgorithm, *args, **kwargs
+) -> float:
+    """Time one run of the naive dynamic algorithm; return time in seconds."""
+    matroid, removal_sequence = setup(*args, **kwargs)
+
+    # start generator (only want to time dynamic part)
+    remover = algorithm(matroid)
+    remover.send(None)
+
+    with Stopwatch() as stopwatch:
+        for element in removal_sequence:
+            remover.send(element)
+
+    return stopwatch.measurement
 
 
-def uniform_weights_dynamic(setup_data: SetupData):
-    elements_to_remove, *_, remover_generator = setup_data
-    return [remover_generator.send(element) for element in elements_to_remove]
+@dataclasses.dataclass(frozen=True)
+class ExperimentConfig:
+    x_variable: tp.Literal["size", "rank"]
+    x_range: tp.Sequence[int]
+    size: int = None
+    rank: int = None
+    uniform_weights: bool = True
 
-plots = {
-    "Total time over exhausting sequence of deletions\n"
-    "uniform weights, fixed size (200), varying rank": (
-        "k (rank of matroid)",
-        range(20, 200, 20),
-        lambda k: setup(size=200, rank=k, uniform_weights=True),
-        [restart_greedy, naive_dynamic, uniform_weights_dynamic],
+    repeats: int = 5
+    normalise_by_size: bool = False
+
+    def __iter__(self) -> tp.Iterable[tp.Dict[str, tp.Any]]:
+        """Iterate over the sequence of algorithm inputs."""
+        for x in self.x_range:
+            yield {
+                "size": self.size,
+                "rank": self.rank,
+                self.x_variable: x,  # overrides one of the above
+                "uniform_weights": self.uniform_weights,
+            }
+
+    def __len__(self):
+        return len(self.x_range)
+
+
+timers = {
+    "restart_greedy": time_restart_greedy,
+    "naive_dynamic": functools.partial(
+        time_dynamic, dynamic_removal_maximal_independent_set
     ),
-    "Total time over exhausting sequence of deletions\n"
-    "uniform weights, varying size, fixed rank (50)": (
-        "n (size of matroid)",
-        range(100, 1000, 100),
-        lambda n: setup(size=n, rank=50, uniform_weights=True),
-        [restart_greedy, naive_dynamic, uniform_weights_dynamic],
+    "uniform_weights_dynamic": functools.partial(
+        time_dynamic, dynamic_removal_maximal_independent_set_uniform_weights
     ),
 }
 
-for title, (x_label, n_range, factory, kernels) in plots.items():
-    results = perfplot.bench(
-        n_range=list(n_range),
-        setup=factory,
-        kernels=kernels,
-        xlabel=x_label,
-        target_time_per_measurement=0.0,  # avoid repetitions, mutable operations
-        equality_check=None,  # for speed
+
+experiments = {
+    "Total time over exhausting sequence of deletions\n"
+    "uniform weights, fixed size (200), varying rank": ExperimentConfig(
+        x_variable="rank",
+        x_range=range(20, 200, 20),
+        size=200,
+    ),
+    "Time per deletion over exhausting sequence of deletions\n"
+    "uniform weights, varying size, fixed rank (50)": ExperimentConfig(
+        x_variable="size",
+        x_range=range(100, 1500, 150),
+        rank=60,
+        normalise_by_size=True,
+    ),
+}
+
+
+plt.style.use(matplotx.styles.dufte)
+
+for title, config in experiments.items():
+    times = np.full(
+        shape=(len(timers), len(config.x_range), config.repeats), fill_value=np.nan
     )
-    results.plot(logx=False, logy=False)
-    plt.title(title)
+
+    for j, input_data in enumerate(tqdm.tqdm(config)):
+        for i, (algorithm_name, timer) in enumerate(timers.items()):
+            for k in range(config.repeats):
+                times[i, j, k] = timer(**input_data)
+
+    if config.normalise_by_size:
+        times /= (
+            np.array(config.x_range)[np.newaxis, :, np.newaxis]
+            if config.x_variable == "size"
+            else config.size
+        )
+
+    means = times.mean(axis=-1)
+    stds = times.std(axis=-1)
+
+    plt.suptitle(title)
+    plt.xlabel(config.x_variable)
+    plt.ylabel("time (s)")
+
+    for i, algorithm_name in enumerate(timers):
+        plt.errorbar(
+            config.x_range, means[i], yerr=stds[i], marker=".", label=algorithm_name
+        )
+
+    plt.gca().set_ylim(bottom=0)
+    plt.legend()
     plt.tight_layout()
     plt.show()
